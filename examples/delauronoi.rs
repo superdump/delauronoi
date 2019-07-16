f830d71c1203a398d1825c98b5e4fcd7d26aeca7
Robert Swain <robert.swain@gmail.com>
2019-07-16 15:10:28 +0200

    Add logging using log and env_logger crates

--- examples/delauronoi.rs
+++ examples/delauronoi.rs
@@ -1,9 +1,17 @@
+#[macro_use]
+extern crate log;
+
 use clap::App;
+use env_logger;
 
 fn main() {
-    let app = App::new("Delauronoi")
+    env_logger::init();
+
+    let _app = App::new("delauronoi")
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .about("Delaunay triangulation of sets of points producing meshes and Voronoi diagrams")
         .get_matches();
+
+    info!("Starting delauronoi");
 }
